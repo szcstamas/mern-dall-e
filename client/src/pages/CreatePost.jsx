e6eb1ad47b6296b49f62602b11c180d8c2f354ac
sztamas99 <szucs.tamas1999@gmail.com>
2023-02-19 18:42:53 +0100

    creating getRandomPrompt so users can use random and different prompts for creating images

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -1,4 +1,10 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { useNavigate } from "react-router-dom";
+
+import { preview } from "../assets";
+//every time we call it, it gives a random prompt that the user can use to generate image 
+import { getRandomPrompt } from "../utils";
+import { FormField, Loader } from "../components";
 
 const CreatePost = () => {
     return (
