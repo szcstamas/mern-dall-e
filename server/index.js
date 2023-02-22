e760b023d9738d480e43bf8ce786fe20f7a1ca05
sztamas99 <szucs.tamas1999@gmail.com>
2023-02-22 20:25:27 +0100

    solving issue where connection to mongo database was rejected / env param was needed / creating Card component

--- server/index.js
+++ server/index.js
@@ -1,37 +1,34 @@
-import express from "express";
-import * as dotenv from "dotenv";
-import cors from "cors";
-import connectDB from "./mongodb/connect.js";
-import postRoutes from "./routes/postRoutes.js";
-import dalleRoutes from "./routes/dalleRoutes.js";
+import express from 'express';
+import * as dotenv from 'dotenv';
+import cors from 'cors';
+
+import connectDB from './mongodb/connect.js';
+import postRoutes from './routes/postRoutes.js';
+import dalleRoutes from './routes/dalleRoutes.js';
+import { env } from "process";
 
 dotenv.config();
 
 const app = express();
-//middlewares
 app.use(cors());
 app.use(express.json({ limit: '50mb' }));
 
-//adding postRoutes to send prompt and retrieve the image
 app.use('/api/v1/post', postRoutes);
-//adding dalleRoutes to generate the data from the api
 app.use('/api/v1/dalle', dalleRoutes);
 
-//verify if server is working
 app.get('/', async (req, res) => {
-    res.send("Hello from DALL-E");
+  res.status(200).json({
+    message: 'Hello from DALL.E!',
+  });
 });
 
-//starting server and connecting to mongoDB
 const startServer = async () => {
-
-    try {
-        connectDB(process.env.MONGODB_URL);
-        app.listen(8080, () => { console.log("Server has started on port http://localhost:8080") });
-    }
-    catch (error) {
-        console.log(error);
-    }
-}
+  try {
+    connectDB(process.env.MONGODB_URL);
+    app.listen(8080, () => console.log('Server started on port 8080'));
+  } catch (error) {
+    console.log(error);
+  }
+};
 
 startServer();
