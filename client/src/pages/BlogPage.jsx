4766a0260742fbeb74ac4b18a7681ba5e5d319fb
sztamas99 <szucs.tamas1999@gmail.com>
2023-03-18 14:21:33 +0100

    creating syntax that renders out ErrorPage when searched ID of blog was not found

--- client/src/pages/BlogPage.jsx
+++ client/src/pages/BlogPage.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { useParams } from 'react-router-dom';
 import { Blog } from '../components';
 import { blogPosts } from '../constants/blogs';
+import ErrorPage from './ErrorPage';
 
 const BlogPage = () => {
     const params = useParams();
@@ -9,20 +10,29 @@ const BlogPage = () => {
 
     //filter out id of blogpost that matches with param
     const filteredBlogPost = blogPosts.filter(blog => blog.id == id);
-    //destructuring given array
-    const [currentBlogObj] = filteredBlogPost;
-    //destructuring given obj
-    const { subline, h1, txt, quotation } = currentBlogObj;
 
-    //returning blogpost
-    return (
-        <Blog
+    //if filtering of blogPosts array was successful, then begin rendering out subpage of recent blog
+    if (filteredBlogPost.length > 0) {
+        //destructuring given array
+        const [currentBlogObj] = filteredBlogPost;
+        //destructuring given obj
+        const { subline, h1, txt, quotation } = currentBlogObj;
+
+        //returning blogpost
+        return <Blog
             subline={subline}
             h1={h1}
             txt={txt}
             quotation={quotation}
         />
-    )
+    } else {
+        //if given array is empty (so filtering was not successful, then render out errorpage)
+        return (
+            <ErrorPage />
+        )
+    }
+
+
 }
 
 export default BlogPage
